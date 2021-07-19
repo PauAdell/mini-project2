e9ae29d426ea95a284477fcd1cf022fc752aa71c
PauAdell <pau.adell1@gmail.com>
2021-07-19 14:58:16 +0200

    ActualitzacióGetPostType

--- nelio-customizations.php
+++ nelio-customizations.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) or die( 'Get out!' );
 function filtrar_titol( $title ) {
 
   if ( is_single() ) {
-    return $title .'<b><em>[Pau]</em></b>';
+    return $title .' <b><em>[Pau]</em></b>';
   } else {
     return $title;
   }
@@ -20,7 +20,7 @@ function filtrar_titol( $title ) {
 add_filter( 'the_title', 'filtrar_titol' );
 
 function imatge_final_entrada ( $content) {
-  if ( is_single() ) {
+  if ( is_single() && get_post_type() == 'post' ) {
     $image_url = untrailingslashit( plugin_dir_url(__FILE__) );
     $image_url .= "/images/gorgeous-winter-snow-wallpaper-44338-45459-hd-wallpapers.jpg";
     $content .='<img src="'.$image_url.'">';
